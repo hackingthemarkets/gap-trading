45c4a4a5c8fb533a8b6375a277531d4e9fd1f936
hackingthemarkets <parttimelarry@gmail.com>
2022-04-20 15:28:35 -0700

    change comments

--- news.py
+++ news.py
@@ -7,6 +7,8 @@ def on_message(ws, message):
         if len(msg) > 0:
             if msg[0] and 'msg' in msg[0] and msg[0]['msg'] == 'authenticated':
                 ws.send(json.dumps({"action":"subscribe","news":["*"]}))
+                # Specific stock and/or crypto symbols #
+                # {"action":"subscribe","news":["AAPL", "TSLA"]}
     except Exception as e:
         print(e)
         print("error with message")
@@ -21,9 +23,6 @@ def on_open(ws):
     print("Opened connection")
     ws.send(json.dumps({"action":"auth","key":config.API_KEY,"secret":config.SECRET_KEY}))
 
-    # Specific stock and/or crypto symbols #
-    # {"action":"subscribe","news":["AAPL", "TSLA"]}
-
 if __name__ == "__main__":
     websocket.enableTrace(True)
     ws = websocket.WebSocketApp("wss://stream.data.alpaca.markets/v1beta1/news",
@@ -33,4 +32,3 @@ if __name__ == "__main__":
                               on_close=on_close)
 
     ws.run_forever()
-
