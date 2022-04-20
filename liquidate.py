062d6e96be6a1b7b03d69cdba5b9f7f7e7f26a8b
hackingthemarkets <parttimelarry@gmail.com>
2022-04-20 15:30:59 -0700

    add liquidate script

---
+++ liquidate.py
@@ -0,0 +1,6 @@
+import config
+from alpaca_trade_api.rest import REST
+
+api = REST(key_id=config.API_KEY, secret_key=config.SECRET_KEY, base_url=config.BASE_URL)
+api.cancel_all_orders()
+api.close_all_positions()
